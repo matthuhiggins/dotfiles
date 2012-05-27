a46e2071a42bba3fc5bf319f590b58a3ed662f29
Matthew Higgins <developer@matthewhiggins.com>
2012-05-27 11:58:19 -0700

    new gitconfig

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@
 ########## Variables
 dir=~/dotfiles                    					# TODO automate
 olddir=~/dotfiles_old             					# TODO move to ./old, gitignore it
-files="ackrc bash_profile git-completion.bash gitconfig"  # TODO Make this dynamic
+files="ackrc bash_profile git-completion.bash gitconfig gitignore_global"  # TODO Make this dynamic
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
@@ -20,8 +20,8 @@ echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+  echo "Moving any existing dotfiles from ~ to $olddir"
+  mv ~/.$file ~/dotfiles_old/
+  echo "Creating symlink to $file in home directory."
+  ln -s $dir/$file ~/.$file
 done
