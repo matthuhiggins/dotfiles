f148b4eabf825a9e943f4e58ac84026cc0e7782a
Matthew Higgins <developer@matthewhiggins.com>
2014-01-08 07:59:55 -0800

    New dotfiles

--- files/chef/knife.rb
+++ files/chef/knife.rb
@@ -1,12 +1,25 @@
+current_dir = File.dirname(__FILE__)
+user_email  = `git config --get user.email`
+home_dir    = ENV['HOME']
+org         = ENV['chef_org'] || 'my_org'
+
+knife_override = "#{home_dir}/.chef/knife_override.rb"
+
 log_level                :info
 log_location             STDOUT
+
 node_name                'matthuhiggins'
-client_key               '~/.chef/matthuhiggins.pem'
-validation_client_name   'gotime-validator'
-validation_key           '~/.chef/gotime.pem'
-chef_server_url          'https://api.opscode.com/organizations/gotime'
-cache_type               'BasicFile'
-cache_options             :path => '~/.chef/checksums'
-cookbook_path             ['~/infogroup/chef-repo/cookbooks', '~/infogroup/chef-repo/site-cookbooks']
+client_key               "#{home_dir}/.chef/#{node_name}.pem"
+
+cookbook_email           "#{user_email}"
+
+cookbook_path    "#{current_dir}/../cookbooks"
+role_path        "#{current_dir}/../roles"
+environment_path "#{current_dir}/../environments"
+
+validation_client_name   'infogroup-validator'
+validation_key           "#{current_dir}/infogroup-validator.pem"
+chef_server_url          'https://chef.prd.data-axle.infogroup.com'
+
 knife[:aws_access_key_id]     = ENV['AMAZON_ACCESS_KEY_ID']
 knife[:aws_secret_access_key] = ENV['AMAZON_SECRET_ACCESS_KEY']
