5cbc33ef2427493ed74cb1d6e11da553a202c6c9
Matthew Higgins <developer@matthewhiggins.com>
2017-10-02 09:00:30 -0700

    Add textmate bundles

---
+++ textmate.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/bomberstudios/Strip-Whitespace-On-Save.tmbundle.git ~/Library/Application\ Support/TextMate/Bundles/Strip-Whitespace-On-Save.tmbundle
+git clone https://github.com/hajder/Ensure-New-Line-at-the-EOF.tmbundle.git ~/Library/Application\ Support/TextMate/Bundles/Ensure-New-Line-at-the-EOF.tmbundle
+
