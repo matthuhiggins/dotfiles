cd376ee074b6adbba833e6c7910bd6f2ea92ad2c
Matthew Higgins <developer@matthewhiggins.com>
2020-05-02 10:56:05 -0700

    Automate elasticsearch

--- brew.sh
+++ brew.sh
@@ -1,7 +1,11 @@
-brew tap heroku/brew
+brew tap heroku/brew \
+         homebrew/cask-versions \
+         elastic/tap
+
 brew install \
   bash-completion \
   coreutils \
+  elastic/tap/elasticsearch-full
   git \
   heroku \
   jenv \
